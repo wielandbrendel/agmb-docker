jupyter/notebook

commit 89b7d96ccd87d03226b7977b469e69172c39ef0e

Fixing way in which selection ranges are deleted

notebook/static/notebook/js/codecell.js
@@ -53,8 +53,9 @@ define([
          if (sel) { 
             var ranges = cm.listSelections();
             for (var i = ranges.length - 1; i >= 0; i--) {
-              var cur = ranges[i].head;
-              cm.replaceRange("", Pos(cur.line, cur.ch - 1), Pos(cur.line, cur.ch + 1));
+              var head = ranges[i].head;
+              var anchor = ranges[i].anchor;
+              cm.replaceRange("", Pos(head.line, head.ch), CodeMirror.Pos(anchor.line, anchor.ch));
             }
             return;
         }

wielandbrendel/agmb-docker

commit f26d5f5314367a60fa807a183682298c3046c2bc

Squashed 'jupyter-notebook/ubuntu-14.04/' changes from a963647..f81113c
f81113c Merge pull request #950 from vbraun/use_default_url
0f0cbc2 Use default_url instead of hardcoding /tree for initial url
96c39cd Merge pull request #948 from Carreau/ceil
6109cdc use math.ceill to allow values < 5s as input
82fb153 Merge pull request #936 from Carreau/release-instructions
8d47271 Merge pull request #938 from willingc/announce
d2ba5b1 Update doc for 4.1 announcement
3124eae Merge pull request #937 from Carreau/dotdev
02b5f46 Add backticks to fix typos
9ab90bd fix regex
6aaa27c and run jsversion
9523f74 dev has a dot
3754c28 back on dev
3fc06a5 Release 4.1.0
5f31251 update release instruction a bit again
ef726c4 Merge pull request #927 from steveholden/patch-1
2617931 Fix "an Jupyter" changes
b410df0 Merge pull request #907 from captainsafia/add-interface-desc-talks
453a432 Merge pull request #924 from willingc/readme-pasta
9b01b0a Addressed PR comments
6d702c4 Correct copy pasta typos
9bd4a49 Merge pull request #923 from anirudhjoshi/master
eb2764b Fixed missing import of to_api_path in example
5275388 Merge pull request #918 from jcb91/notebook/js/codecell
7bfeea4 Merge pull request #915 from minrk/right-soft-selected
53b8e37 fix for #917, a bug introduced by a typo in 89b7d96ccd87d03226b7977b469e69172c39ef0e
0ec5164 Updated table of contents
66aa256 Added UI components page
b9a9b0d Added user interface screenshots
d86714a Merge pull request #914 from minrk/check-origin
308cbb2 Remove soft selected right border background-leaking
b2d9faa Merge pull request #909 from Carreau/left-soft-selected
1257a3a Merge pull request #905 from minrk/revert-configurable-config-dir
8aa4f1e allow zmq check_origin with no args
bf6c89b Merge pull request #897 from Carreau/warn-name-ations
7abc4e8 Merge pull request #910 from Carreau/fix-#904
ef0f3b9 Do not allow `<` in url.
cc69f06 Remove left border on softselected cells
a5de8f2 Added initial annotated image

docs/source/examples/Notebook/Notebook Basics.ipynb
@@ -197,7 +197,7 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "Command mode is indicated by a grey cell border:\n",
+    "Command mode is indicated by a grey cell border with a blue left margin:\n",
     "\n",
     "<img src=\"images/command_mode.png\">\n",
     "\n",
@@ -291,7 +291,7 @@
  ],
  "metadata": {
   "kernelspec": {
-   "display_name": "Python 3",
+   "display_name": "IPython (Python 3)",
    "language": "python",
    "name": "python3"
   },
@@ -305,7 +305,7 @@
    "name": "python",
    "nbconvert_exporter": "python",
    "pygments_lexer": "ipython3",
-   "version": "3.4.0"
+   "version": "3.5.1"
   }
  },
  "nbformat": 4,

notebook/_version.py
@@ -9,5 +9,5 @@ store the current version info of the notebook.
 
 # Next beta/alpha/rc release: The version number for beta is X.Y.ZbN **without dots**. 
 
-version_info = (4, 1, 0, 'rc1')
+version_info = (4, 2, 0, '.dev')
 __version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])

notebook/base/zmqhandlers.py
@@ -101,6 +101,7 @@ class WebSocketMixin(object):
     ping_callback = None
     last_ping = 0
     last_pong = 0
+    stream = None
     
     @property
     def ping_interval(self):
@@ -120,7 +121,7 @@ class WebSocketMixin(object):
             max(3 * self.ping_interval, WS_PING_INTERVAL)
         )
 
-    def check_origin(self, origin):
+    def check_origin(self, origin=None):
         """Check Origin == Host or Access-Control-Allow-Origin.
         
         Tornado >= 4 calls this method automatically, raising 403 if it returns False.
@@ -129,7 +130,9 @@ class WebSocketMixin(object):
             return True
 
         host = self.request.headers.get("Host")
-
+        if origin is None:
+            origin = self.get_origin()
+        
         # If no header is provided, assume we can't verify origin
         if origin is None:
             self.log.warn("Missing Origin header, rejecting WebSocket connection.")

notebook/notebookapp.py
@@ -827,7 +827,8 @@ class NotebookApp(JupyterApp):
         )
         self.config_manager = self.config_manager_class(
             parent=self,
-            log=self.log
+            log=self.log,
+            config_dir=os.path.join(self.config_dir, 'nbconfig'),
         )
 
     def init_logging(self):
@@ -1095,7 +1096,7 @@ class NotebookApp(JupyterApp):
                 relpath = os.path.relpath(self.file_to_run, self.notebook_dir)
                 uri = url_escape(url_path_join('notebooks', *relpath.split(os.sep)))
             else:
-                uri = 'tree'
+                uri = self.default_url
             if browser:
                 b = lambda : browser.open(url_path_join(self.connection_url, uri),
                                           new=2)

notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "4.1.0rc1";
+    Jupyter.version = "4.2.0.dev";
     Jupyter._target = '_blank';
     return Jupyter;
 });

notebook/static/base/js/utils.js
@@ -370,7 +370,7 @@ define([
 
     // Locate any URLs and convert them to a anchor tag
     function autoLinkUrls(txt) {
-        return txt.replace(/(^|\s)(https?|ftp)(:[^'">\s]+)/gi,
+        return txt.replace(/(^|\s)(https?|ftp)(:[^'"<>\s]+)/gi,
             "$1<a target=\"_blank\" href=\"$2$3\">$2$3</a>");
     }
 

notebook/static/notebook/js/actions.js
@@ -18,6 +18,16 @@
 define(function(require){
     "use strict";
 
+    var warn_bad_name = function(name){
+        if(name !== "" && !name.match(/:/)){
+            console.warn('You are trying to use an action/command name, where the separator between prefix and name is not `:`\n'+
+                         '"'+name+'"\n'+
+                         'You are likely to not use the API in a correct way. Typically use the following:\n'+
+                         '`var key = actions.register(<object>, "<name>", "<prefix>");` and reuse the `key` variable'+
+                         'instead of re-generating the key yourself.'
+                    );
+        }
+    };
 
     var ActionHandler = function (env) {
         this.env = env || {};
@@ -620,9 +630,9 @@ define(function(require){
 
         for(k in custom_ignore){
             // Js closure are function level not block level need to wrap in a IIFE
-            // same as above, but decide for themselves wether or not they intercept events.
+            // same as above, but decide for themselves whether or not they intercept events.
             if(custom_ignore.hasOwnProperty(k)){
-                var handler = _prepare_handler(final_actions, k, custom_ignore);
+                handler = _prepare_handler(final_actions, k, custom_ignore);
                 (function(key, handler){
                     final_actions['jupyter-notebook:'+key].handler = function(env, event){
                         return handler(env, event);
@@ -693,6 +703,7 @@ define(function(require){
          **/
 
         if(typeof(name_or_data) === 'string'){
+            warn_bad_name(name);
             if(this.exists(name_or_data)){
                 return name_or_data;
             } else {
@@ -704,6 +715,7 @@ define(function(require){
     };
 
     ActionHandler.prototype.get = function(name){
+        warn_bad_name(name);
         return this._actions[name];
     };
 

notebook/static/notebook/js/codecell.js
@@ -50,12 +50,12 @@ define([
      */
     CodeMirror.commands.delSpaceToPrevTabStop = function(cm){
         var from = cm.getCursor(true), to = cm.getCursor(false), sel = !posEq(from, to);
-         if (sel) { 
+         if (sel) {
             var ranges = cm.listSelections();
             for (var i = ranges.length - 1; i >= 0; i--) {
-              var head = ranges[i].head;
-              var anchor = ranges[i].anchor;
-              cm.replaceRange("", Pos(head.line, head.ch), CodeMirror.Pos(anchor.line, anchor.ch));
+                var head = ranges[i].head;
+                var anchor = ranges[i].anchor;
+                cm.replaceRange("", CodeMirror.Pos(head.line, head.ch), CodeMirror.Pos(anchor.line, anchor.ch));
             }
             return;
         }

notebook/static/notebook/js/notebook.js
@@ -2518,8 +2518,9 @@ define(function (require) {
         if (this.autosave_interval) {
             // new save interval: higher of 10x save duration or parameter (default 30 seconds)
             var interval = Math.max(10 * duration, this.minimum_autosave_interval);
-            // round to 10 seconds, otherwise we will be setting a new interval too often
-            interval = 10000 * Math.round(interval / 10000);
+            // ceil to 10 seconds, otherwise we will be setting a new interval too often
+            // do not round or anything below 5000ms will desactivate saving.
+            interval = 10000 * Math.ceil(interval / 10000);
             // set new interval, if it's changed
             if (interval !== this.autosave_interval) {
                 this.set_autosave_interval(interval);

notebook/static/notebook/js/outputarea.js
@@ -684,7 +684,7 @@ define([
                 img.addClass('unconfined');
             }
         });
-    };
+    }
     
     var set_width_height = function (img, md, mime) {
         /**

notebook/static/notebook/js/quickhelp.js
@@ -189,7 +189,7 @@ define([
             'The Jupyter Notebook has two different keyboard input modes. <b>Edit mode</b> '+
             'allows you to type code/text into a cell and is indicated by a green cell '+
             'border. <b>Command mode</b> binds the keyboard to notebook level actions '+
-            'and is indicated by a grey cell border.'
+            'and is indicated by a grey cell border with a blue left margin.'
         );
         element.append(doc);
         if (platform === 'MacOS') {

notebook/static/notebook/less/cell.less
@@ -27,7 +27,11 @@ div.cell {
     &.jupyter-soft-selected {
         border-left-color: @selected_border_color_light;
         border-left-color: @soft_select_color;
-        ._selected_style(@selected_border_color_light, @soft_select_color, 5px, 0px);
+        border-left-width: @cell_border_width;
+        padding-left: @cell_padding - @cell_border_width;
+        border-right-color: @soft_select_color;
+        border-right-width: @cell_border_width;
+        background: @soft_select_color;
 
         @media print {
             border-color: transparent;

notebook/tests/test_notebookapp.py
@@ -101,7 +101,7 @@ def test_pep440_version():
 
 
 
-pep440re = re.compile('^(\d+)\.(\d+)\.(\d+((a|b|rc)\d+)?)(\.post\d+)?(\.dev\d+)?$')
+pep440re = re.compile('^(\d+)\.(\d+)\.(\d+((a|b|rc)\d+)?)(\.post\d+)?(\.dev\d*)?$')
 
 def raise_on_bad_version(version):
     if not pep440re.match(version):